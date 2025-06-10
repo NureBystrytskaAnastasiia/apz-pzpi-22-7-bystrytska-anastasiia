1eb906e321944e31cba5e1d5997ce4e879797d71
NureBystrytskaAnastasiia <anastasiia.bystrytska@nure.ua>
2025-06-10 13:33:54 +0300

    Delete Lab2/pzpi_22_7_bystrytska_anastasiia_lab2 directory

--- Lab2/pzpi_22_7_bystrytska_anastasiia_lab2/models/Room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const roomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: '' },
-  temperature: { type: Number, default: null },
-  humidity: { type: Number, default: null },
-  updatedAt: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('Room', roomSchema);
--- Lab2/pzpi_22_7_bystrytska_anastasiia_lab2/models/SensorData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const sensorDataSchema = new mongoose.Schema({
-  roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
-  temperature: { type: Number, required: true },
-  humidity: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now, expires: '7d' },
-});
-module.exports = mongoose.model('SensorData', sensorDataSchema);
