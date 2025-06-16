c21ced028a27949f7d7143c17369b2f0e0daf382
NureBystrytskaAnastasiia <anastasiia.bystrytska@nure.ua>
2025-06-16 03:09:38 +0300

    Add files via upload

---
+++ Lab4/pzpi-22-7-bystrytska-anastasiia-lab4/data/model/RoomResponse.kt
@@ -0,0 +1,6 @@
+package com.example.mobile.data.model
+
+data class RoomResponse(
+    val count: Int,
+    val rooms: List<Room>
+)
---
+++ Lab4/pzpi-22-7-bystrytska-anastasiia-lab4/data/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.mobile.data.model
+
+data class User(
+    val _id: String,
+    val email: String,
+    val name: String,
+    val role: String
+)
